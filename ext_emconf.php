9d5ce5347a566a6b10113e7042c074fc450cbdc9
Marco Wegner <m.wegner@remind.de>
2020-03-26 10:49:23 +0100

    Initial commit gitignore, composer.json, ext_emconf, .build

---
+++ ext_emconf.php
@@ -0,0 +1,16 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'REMIND - Hybridauth',
+    'description' => 'Connect TYPO3 frontend login with hybridauth',
+    'category' => 'misc',
+    'version' => '1.0.0',
+    'state' => 'stable',
+    'clearcacheonload' => 1,
+    'author' => 'Marco Wegner',
+    'author_email' => 'm.wegner@remind.de',
+    'author_company' => 'REMIND GmbH',
+    'constraints' => [
+        'depends' => []
+    ]
+];
